1e82da2d5cdca7d5ec3f13457d99e0996bb20e77
cosmos45 <zendeomkar45@gmail.com>
2021-03-28 01:21:25 +0530

    firstt

---
+++ build/precache-manifest.da314c8722ce3a84d523c3f7d51ca31a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbd68eddefbfed8c7f354a62d725d854",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d2fc04e4f745e085d53",
+    "url": "/static/css/main.7e777dc0.chunk.css"
+  },
+  {
+    "revision": "d90f0e03a4f4398a36f4",
+    "url": "/static/js/2.3049cd24.chunk.js"
+  },
+  {
+    "revision": "1fe3ec75400f0361a43c3cf73a205519",
+    "url": "/static/js/2.3049cd24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d2fc04e4f745e085d53",
+    "url": "/static/js/main.83a5c87a.chunk.js"
+  },
+  {
+    "revision": "11ea2196c4c37055d395",
+    "url": "/static/js/runtime-main.a881be68.js"
+  }
+]);
